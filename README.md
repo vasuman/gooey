0c036c66abfb6353e8b461f1c43b323002703d56
Vasuman <varav@varav.in>
2016-04-08 19:48:41 +0530

    Contract linking

--- README.md
+++ README.md
@@ -19,6 +19,55 @@ contracts/
         ContractB.sol
     ...
 ```
+
+The tool will automatically pick up all files with the `.sol` extension (use the
+`-e` option to change).
+
+## Contract linking
+
+Since contracts deployed on the blockchain are immutable, it is recommended to
+split your logic into multiple contracts and link them together. When you want
+to fix a bug, you simply change the required contracts and link your new
+contracts.
+
+### Configuration
+
+You need to setup a deployment configuration first. Let's assume that we have a
+file called `config.json` (use the `-c` option to point to another file) that
+looks like,
+
+```
+{
+    "ContractA": {
+        "from": "0xc0de..."
+    },
+    "ContractB": {
+        "inject": [{
+            "ref": "ContractA",
+            "method": "setA"
+        }]
+    }
+}
+```
+
+The `inject` section of each contract is a list of dependencies that need to be
+injected to the contract. Each dependency lists a `ref` and `method`. The `ref`
+is the name of the contract that you want to link. The `method` is the name of
+the method that updates the linked contract address. In the above example,
+ideally, the `setA` method will look something like,
+
+```
+contract ContractB {
+    ContractA a;
+
+    ...
+
+    function setA(ContractA _a) {
+        a = _a;
+    }
+}
+```
+
 ## Usage
 
 Invoke the `gooey` command and supply the path to the directory containing your
@@ -45,51 +94,32 @@ change the name) in your contracts directory structured like,
 }
 ```
 
-Subsequent invocation of the command will read from this file to determine
-whether the contract has changed.
-
 Use the `-s` option to maintain seperate *state files* for your testnet and
-homestead deployments. Like,
+homestead deployments.
 
-```
-$ gooey -s test contracts/
-```
-
-### Deploy
-
-You can specify the `-d` option to deploy your contracts. You need to setup a
-deployment configuration first. Let's assume that we have a file called
-`deploy.json` that looks like,
-
-```
-{
-    "ContractA": {}
-}
-```
-
-Invoke `gooey`,
-
-```
-$ gooey -d deploy.json contracts/
-```
-
-The *state file* entry for your contract will have an additional field
-`address`, that gives you the address on the blockchain to which the contract
-was deployed.
+If you choose to deploy your contracts (happens by default unless you use the
+`-d` option which basically performs a dry run), the *state file* entry for your
+contract will have an additional field `address`, that gives you the address on
+the blockchain to which the contract was deployed.
 
 ```
 {
     "ContractA": {
         ...
-        "address": "0xdeadbeef...."
-    }
+        "address": "0xdeadbeef..."
+    },
+    ...
 }
 ```
+
+Subsequent invocation of the command will read from this file to determine
+whether the contract has changed.
 
 Contracts are only **re-deployed** if their code has changed.
 
 ## Todo
 
 - [x] Utilities for testing
-- [ ] Support contract linking
-- [ ] Configurable `from` address
+- [x] Support contract linking
+- [x] Configurable `from` address
+- [ ] Refactor and cleanup
--- bin/gooey.js
+++ bin/gooey.js
@@ -3,107 +3,37 @@
 'use strict';
 
 const fs = require('fs');
-const path = require('path');
 
 const cmd = require('commander');
 const Web3 = require('web3');
 
-const gooey = require('../index.js');
+const contracts = require('../lib/contracts.js');
+const utils = require('../lib/utils.js');
 
 cmd
-  .option('-a --address', 'RPC address of local ethereum node',
+  .option('-a --address <addr>', 'RPC address of local ethereum node',
           'http://localhost:8545')
-  .option('-d --deploy <config>', 'Deploy contracts on blockchain', '')
-  .option('-s --state <name>', 'Base state file name', 'main')
+  .option('-c --config <config>', 'Configuration file', 'deploy.json')
+  .option('-s --state <name>', 'State file name', 'state.json')
   .option('-e --ext <extension>', 'Extension of contract files', '.sol')
+  .option('-d --dry-run', 'Skip contract deployment')
   .arguments('<contract-dir>')
   .action(main)
   .parse(process.argv);
 
-function compileAll(contractDir) {
-  let sources = {};
-
-  function recurse(dir) {
-    let files = fs.readdirSync(dir);
-    for (let f of files) {
-      let full = path.join(dir, f);
-      if (fs.statSync(full).isDirectory()) {
-        recurse(full);
-      } else if (f.endsWith(cmd.ext)) {
-        let src = fs.readFileSync(full, 'utf8');
-        console.log(`compiling file, ${full}`);
-        sources[full] = src;
-      }
-    }
-  }
-
-  recurse(contractDir);
-  return gooey.contracts.compile(sources, true);
-}
-
-function merge(dir, contracts) {
-  let fPath = path.join(dir, `${cmd.state}.json`);
-  let existing = null;
-
-  function write() {
-    fs.writeFileSync(fPath, JSON.stringify(contracts, null, 4), 'utf8');
-  }
-
-  function exists(name, code) {
-    return existing && existing[name] && existing[name].code === code;
-  }
-
-  function isDeployed(name, code) {
-    return exists(name, code) && existing[name].address;
-  }
-
-  try {
-    existing = JSON.parse(fs.readFileSync(fPath, 'utf8'));
-  } catch (e) {}
-
-  for (let name of Object.keys(contracts)) {
-    if (isDeployed(name, contracts[name].code)) {
-      contracts[name].address = existing[name].address;
-    }
-  }
-
-  if (!cmd.deploy) {
-    write();
-    return;
-  }
-
-  let cfg = JSON.parse(fs.readFileSync(cmd.deploy), 'utf8');
-  let ps = [];
-  let web3 = new Web3(new Web3.providers.HttpProvider(cmd.address));
-  for (let name of Object.keys(contracts)) {
-    if (name in cfg) {
-      if (contracts[name].address) {
-        console.log(`contract ${name} already deployed`);
-        continue;
-      }
-      // TODO: contract linking
-      console.log(`deploying contract, ${name}`);
-      let abi = contracts[name].abi;
-      let code = contracts[name].code;
-      let p = gooey.contracts.deploy(web3, web3.eth.coinbase, abi, code, []);
-      ps.push(p.then(c => {
-        console.log(`contract ${name} deployed at ${c.address}`);
-        contracts[name].address = c.address;
-      }));
-      Promise.all(ps).then(write).catch(console.error);
-    }
-  }
-}
-
 function main(contractDir) {
-  let compiled = compileAll(contractDir);
-  let contracts = {};
-  for (let name of Object.keys(compiled)) {
-    let contract = compiled[name];
-    contracts[name] = {
-      abi: JSON.parse(contract.interface),
-      code: contract.bytecode
-    };
-  }
-  merge(contractDir, contracts);
+  let web3 = new Web3(new Web3.providers.HttpProvider(cmd.address));
+  let sources = utils.getSources(contractDir, cmd.ext);
+  let compiled = contracts.compile(sources, true);
+  let prev = {};
+  try {
+    prev = JSON.parse(fs.readFileSync(cmd.state, 'utf8'));
+  } catch (e) {}
+  let config = JSON.parse(fs.readFileSync(cmd.config, 'utf8'));
+  contracts.execute(web3, config, prev, compiled, cmd.dryRun).then(state => {
+    fs.writeFileSync(cmd.state, JSON.stringify(state, null, 2), 'utf8');
+  }).catch(reason => {
+    console.error(`failed because: ${reason}`);
+    process.exit(1);
+  });
 }
